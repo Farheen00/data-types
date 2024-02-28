3d2c0b2c58e7e28b59df8ea55d793c194700e892
Farheen00 <159334516+Farheen00@users.noreply.github.com>
2024-02-28 00:40:51 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// Example String Data Type
+var personName = "Farheen";
+console.log(personName);
+//Example String Data Type with concatenation
+var myName = "Farheen";
+console.log("Hello " + myName + "!");
+// Example Number Data Type
+var age = 33;
+console.log(age);
+// Example Number Data Type with arithmetic operators
+var x = 10;
+var y = 5;
+console.log(x + y); //Addition
+console.log(x - y); //Subtraction
+console.log(x * y); //Multiplication
+console.log(x / y); //Division
+console.log(x % y); //Modulus
+// Example 3 :Boolean Data Type
+var isStudent = true;
+console.log(isStudent);
+// Example Null Data Types
+var b = null;
+console.log(b);
+// Example Undefined  Data Types
+var c = undefined;
+console.log(c);
+var message;
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,37 @@
+// Example String Data Type
+let personName: string = "Farheen";
+console.log(personName);
+
+//Example String Data Type with concatenation
+let myName: string = "Farheen";
+console.log("Hello " + myName + "!");
+
+
+// Example Number Data Type
+let age: number = 33;
+console.log(age);
+
+// Example Number Data Type with arithmetic operators
+let x: number = 10
+let y: number = 5
+console.log(x+y); //Addition
+console.log(x-y); //Subtraction
+console.log(x*y); //Multiplication
+console.log(x/y); //Division
+console.log(x%y)
+console.log(x**y)
+
+// Example 3 :Boolean Data Type
+let isStudent: boolean = true;
+console.log(isStudent);
+
+// Example Null Data Types
+let b: null = null;
+console.log(b);
+
+// Example Undefined  Data Types
+let c: undefined = undefined;
+console.log(c);
+
+let message; 
+console.log(message);
